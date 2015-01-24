620fe39317f89ed09e4d2313eb155016d676c2f8
mac <macattackk@gmail.com>
2015-01-24 10:31:19 -0800

    More Scripts

---
+++ what_do_we_do_now/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class EnemyBehavior : MonoBehaviour
+{
+	public PlayerController player;
+
+	public Transform myTransform,
+	playerTransform;
+
+	protected virtual void Start()
+	{
+		
+	}
+}
