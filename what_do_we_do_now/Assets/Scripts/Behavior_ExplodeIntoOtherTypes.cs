7b86a2e21506943424f95a5b9cc6fc2fa06cd7ef
Victor Lau <vicwlau@gmail.com>
2015-01-24 14:30:37 -0800

    Added GameController and ExplodeIntoOtherTypes

---
+++ what_do_we_do_now/Assets/Scripts/Behavior_ExplodeIntoOtherTypes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Behavior_ExplodeIntoOtherTypes : EnemyBehavior {
+
+	public int numberOfSplits = 3;
+
+	void OnTriggerEnter(Collider other){
+
+		if(other.tag == "Player"){
+
+			ExplodeIntoOtherTypes(numberOfSplits);
+		}
+	}
+
+	public void ExplodeIntoOtherTypes(int numOfTypes){
+
+		for (int i = 0; i < numberOfSplits; i++) {
+
+			GameController.c.CreateRandomEnemyAtPos(myTransform.position + new Vector3(Random.Range(0,5f), 0, Random.Range(0,5f)));
+
+
+		}
+
+		Destroy (this.gameObject);
+	}
+}
