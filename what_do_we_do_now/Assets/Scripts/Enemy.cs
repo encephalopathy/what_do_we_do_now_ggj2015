e429038af687ea283c12d4d66f7a2007d2d0f447
mac <macattackk@gmail.com>
2015-01-24 15:59:17 -0800

    added some enemy scripts

---
+++ what_do_we_do_now/Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour
+{
+	private List<Vector3> hardPoints = new List<Vector3>();
+
+	public EnemyController enemyCon;
+
+	private void Start()
+	{
+		int spreadAmount = 5;
+
+		hardPoints.Add(transform.position);
+		hardPoints.Add(new Vector3(transform.position.x,transform.position.y + spreadAmount,transform.position.z));
+		hardPoints.Add(new Vector3(transform.position.x,transform.position.y + spreadAmount * 2,transform.position.z));
+	}
+
+	private void Update()
+	{
+
+	}
+
+	public void GenerateBodyParts()
+	{
+		int bodyPartAmount = 2;
+
+		if(Random.Range(0,1f) < 0.35f)
+			bodyPartAmount = 3;
+
+		for(int i=0; i<bodyPartAmount; i++)
+		{
+			GameObject newBodyPart = enemyCon.bodyParts[Random.Range(0,enemyCon.bodyParts.Count)];
+
+			Instantiate(newBodyPart,hardPoints[bodyPartAmount],newBodyPart.transform.rotation);
+		}
+	}
+}
---
+++ what_do_we_do_now/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyController : MonoBehaviour
+{
+	public List<GameObject> bodyParts = new List<GameObject>();
+
+	private void Start()
+	{
+
+	}
+
+
+}
