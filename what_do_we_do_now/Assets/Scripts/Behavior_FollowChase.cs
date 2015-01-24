d282aedbf804bf05bca399e363c11a5e18f884f7
mac <macattackk@gmail.com>
2015-01-24 15:51:34 -0800

    some enemy controller stuff

--- what_do_we_do_now/Assets/Scripts/Behavior_FollowChase.cs
+++ what_do_we_do_now/Assets/Scripts/Behavior_FollowChase.cs
@@ -15,7 +15,7 @@ public class Behavior_FollowChase : EnemyBehavior
 
 	private void Update()
 	{
-		if(Vector3.Distance(myTransform.position,playerTransform.position) < 10f)
+		if(!player.bInvisible && Vector3.Distance(myTransform.position,playerTransform.position) < 10f)
 		{
 			if(!bChase)
 				FollowTarget(playerTransform);
@@ -36,8 +36,8 @@ public class Behavior_FollowChase : EnemyBehavior
 
 	public void ChaseTarget(Transform target)
 	{
-//		Vector3 moveToPos = (target.position - myTransform.position);
-//		
-//		MoveToTarget(moveToPos * speed);
+		Vector3 followPosition = Vector3.MoveTowards(myTransform.position,target.position,Time.deltaTime * speed);
+		
+		myTransform.position = new Vector3(followPosition.x,myTransform.position.y,followPosition.z);
 	}
 }
