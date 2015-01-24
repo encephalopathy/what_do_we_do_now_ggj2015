741fc2ee42cedcc95682621030ed45eefb61e8a6
Victor Lau <vicwlau@gmail.com>
2015-01-23 23:08:16 -0800

    Added player controller, GUIcontroller and FollowEnemy. Updated tags and added several new objects.

---
+++ what_do_we_do_now/Assets/Scripts/GUIController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+	public PlayerController playerC;
+
+	void Awake(){
+
+		playerC = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+
+	void Update(){
+
+		ButtonClickActions();
+	}
+
+
+	private void ButtonClickActions(){
+
+		Vector3 _direction = Vector3.zero;
+		
+		if (Input.GetKey (KeyCode.DownArrow)){
+			_direction += Vector3.back;
+		}
+		
+		if (Input.GetKey (KeyCode.UpArrow)){
+			_direction += Vector3.forward;
+		}
+		
+		if (Input.GetKey (KeyCode.LeftArrow)){
+			_direction += Vector3.left;
+		}
+		
+		if (Input.GetKey (KeyCode.RightArrow)){
+			_direction += Vector3.right;
+		}
+
+		playerC.MoveToLocation(_direction);
+	}
+
+}
---
+++ what_do_we_do_now/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MoveToLocation(Vector3 _direction){
+
+		transform.Translate(_direction);
+
+	}
+}
