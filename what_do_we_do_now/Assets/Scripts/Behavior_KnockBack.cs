5d6b1b12c7424184e40bb5cc4495085ed495a768
Victor Lau <vicwlau@gmail.com>
2015-01-24 15:40:53 -0800

    Added knock back and modified player controller

---
+++ what_do_we_do_now/Assets/Scripts/Behavior_KnockBack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Behavior_KnockBack : EnemyBehavior {
+
+	public float knockBackForce = 0.5f;
+
+	void OnTriggerEnter(Collider other){
+
+		if(other.tag == "Player"){
+			
+			player.StartCoroutine (KnockBackSequence());
+		}
+	}
+
+	public IEnumerator KnockBackSequence(){
+
+		if(player.isKnockedBack) yield break;
+
+		player.isKnockedBack = true;
+
+		Vector3 _direction = (playerTransform.position - myTransform.position).normalized; 
+		_direction = new Vector3(_direction.x, 0f, _direction.z);
+		float _timerLength = 1.5f;
+		float _curTimer = 0f;
+
+		while(_curTimer <= _timerLength){
+			_curTimer+= Time.deltaTime;
+			playerTransform.Translate(_direction * knockBackForce * (_timerLength - _curTimer));
+			
+			yield return new WaitForFixedUpdate();
+		}
+
+
+
+	}
+
+}
